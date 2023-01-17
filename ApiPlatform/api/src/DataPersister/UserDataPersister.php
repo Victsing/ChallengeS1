ecc1fe65d994ecf7ac72ae34cbe62968532a748e
Pierre <victsing@outlook.fr>
2023-01-17 11:42:52 +0100

    update register route

---
+++ ApiPlatform/api/src/DataPersister/UserDataPersister.php
@@ -0,0 +1,10 @@
+<? php
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+
+class UserDataPersister implements DataPersisterInterface {
+
+    public function supports($data): bool
+    {
+        return $data instanceof User;
+    }
+}
