00e06d59ee4dbd41abb37d3e94fa4f75c802216a
Locman Diawadoh <locman@locman.fr>
2023-01-19 19:51:03 +0100

    landing page added

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Authentication from '@/views/UserAuthentification.vue'
 import ResetPassword from '@/views/ResetPassword.vue'
+import LandingPage from '@/views/LandingPage.vue'
 
 const routes = [
   {
@@ -10,11 +11,8 @@ const routes = [
     children: [
       {
         path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        name: 'LandingPage',
+        component: LandingPage
       },
       {
         path: '/authentication',
