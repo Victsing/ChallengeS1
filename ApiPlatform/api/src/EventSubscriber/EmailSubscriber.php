7a9cb9d1a9323637475372ae39d87414caeca2e3
Pierre <victsing@outlook.fr>
2023-01-19 14:38:38 +0100

    create EmailSuscriber event

--- ApiPlatform/api/src/EventSubscriber/EmailSubscriber.php
+++ ApiPlatform/api/src/EventSubscriber/EmailSubscriber.php
@@ -12,25 +12,38 @@ use Symfony\Component\HttpKernel\Event\ViewEvent;
 use ApiPlatform\Symfony\EventListener\EventPriorities;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-final class UserSubscriber implements EventSubscriberInterface
+final class EmailSubscriber implements EventSubscriberInterface
 {
+    private mailer $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::VIEW => ['updatePassword', EventPriorities::PRE_WRITE]
+            KernelEvents::VIEW => ['sendEmail', EventPriorities::POST_WRITE]
         ];
     }
-
-    public function updatePassword(ViewEvent $event): void
+    public function sendEmail(ViewEvent $event): void
     {
         $user = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
         dump($event->getRequest());
 
-        if ($user instanceof User && ($method === "POST" || $method=== "PATCH")) {
-            $user->setPassword(password_hash($user->getPassword(), PASSWORD_BCRYPT));
+        if (!$user instanceof User || Request::METHOD_POST !== $method) {
+            return;
         }
+        $message = (new TemplatedEmail())
+            ->from('ChallengeS1ESGI@gmail.com')
+            ->to($user->getEmail())
+            ->subject('test')
+            ->text('Test')
+            ->htmlTemplate('confirmation_email.html.twig');
+        $this->mailer->send($message);
     }
 
 }
