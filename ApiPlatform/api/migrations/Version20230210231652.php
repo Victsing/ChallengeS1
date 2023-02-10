e047c4a8fb6b9b86f735c7f1d9a115c8ded5ca93
Boukhatem <mboukhatem777@gmail.com>
2023-02-11 00:18:14 +0100

    Api platform : Supression d'une Entity et son Repository

---
+++ ApiPlatform/api/migrations/Version20230210231652.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230210231652 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE offer_id_seq CASCADE');
+        $this->addSql('DROP TABLE offer');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SEQUENCE offer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE offer (id INT NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, type_contrat VARCHAR(50) NOT NULL, duration VARCHAR(50) DEFAULT NULL, year_experience INT NOT NULL, PRIMARY KEY(id))');
+    }
+}
