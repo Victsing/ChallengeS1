15e9e2e085e9bcab1bebd356e3a3ed98a9e11a1a
Clarence Potel <potel.clarence@gmail.com>
2023-02-12 13:15:50 +0100

    fix creation d'entreprise

--- vue-app/src/views/employer/NewCompanyJob.vue
+++ vue-app/src/views/employer/NewCompanyJob.vue
@@ -1,66 +1,20 @@
 <template>
-  <BaseNaveBar
-    :title="`Welcome ${decoded.firstname} ${decoded.lastname}`"
-    :employer="isEmployer"
-  />
+  <BaseNaveBar :title="`Welcome ${decoded.firstname} ${decoded.lastname}`" :employer="isEmployer" />
   <h1>New company job</h1>
   <v-form>
-    <v-text-field
-      v-model="job.title"
-      label="Title"
-      required
-    />
-    <v-text-field
-      v-model="job.description"
-      label="Description"
-      required
-    />
-    <v-text-field
-      v-model="job.city"
-      label="City"
-      required
-    />
-    <v-text-field
-      v-model="job.country"
-      label="Country"
-      required
-    />
-    <v-select
-      v-model="job.contractType"
-      :items="contractTypes"
-      label="Contract Type"
-      required
-    />
-    <v-text-field
-      v-model="job.salary"
-      type="number"
-      label="Salary"
-      required
-    />
-    <v-text-field
-      v-model="job.missionDuration"
-      label="duration"
-      required
-    />
-    <v-btn
-      @click="isEditRoute ? updateJob() : addJob()"
-      color="primary"
-      :disabled="disableButton"
-    >
-      <div v-if="isEditRoute">
-        Update
-      </div>
-      <div v-else>
-        Create
-      </div>
+    <v-text-field v-model="job.title" label="Title" required />
+    <v-text-field v-model="job.description" label="Description" required />
+    <v-text-field v-model="job.city" label="City" required />
+    <v-text-field v-model="job.country" label="Country" required />
+    <v-select v-model="job.contractType" :items="contractTypes" label="Contract Type" required />
+    <v-text-field v-model="job.salary" type="number" label="Salary" required />
+    <v-text-field v-model="job.missionDuration" label="duration" required />
+    <v-btn @click="isEditRoute ? updateJob() : addJob()" color="primary" :disabled="disableButton">
+      <div v-if="isEditRoute">Update</div>
+      <div v-else>Create</div>
     </v-btn>
   </v-form>
-  <BaseSnackbar
-    v-model="snackbar"
-    :text="snackbarText"
-    :color="snackbarColor"
-    @close-snackbar="snackbar = false"
-  />
+  <BaseSnackbar v-model="snackbar" :text="snackbarText" :color="snackbarColor" @close-snackbar="snackbar = false" />
 </template>
 
 <script setup>
@@ -68,7 +22,7 @@ import { ref, onMounted, computed } from 'vue';
 import CompanyApi from '@/backend/CompanyApi';
 import AuthentificationApi from '@/backend/AuthentificationApi';
 import JobsApi from '@/backend/JobsApi';
-import BaseNaveBar from '@/components/BaseNaveBar.vue'
+import BaseNaveBar from '@/components/BaseNaveBar.vue';
 import jwt_decode from 'jwt-decode';
 import { useRoute, useRouter } from 'vue-router';
 import BaseSnackbar from '@/components/BaseSnackbar.vue';
@@ -94,11 +48,11 @@ let job = ref({
   contractType: '',
   salary: null,
   missionDuration: '',
-  company: `/companies/${route.params.id}`,
+  company: `/companies/${route.params.id}`
 });
 
 const displayPremium = computed(() => {
-  return !me.value.premium && jobsCount == 2;
+  return !me.value.premium && jobsCount.value == 2;
 });
 
 const isEditRoute = computed(() => {
@@ -108,22 +62,28 @@ const isEditRoute = computed(() => {
 onMounted(() => {
   if (route.name.includes('Edit')) {
     const jobid = isEmployer ? route.params.jobId : route.params.id;
-    JobsApi.getJob(jobid).then((response) => {
-      job.value = response.data;
-    }).catch((error) => {
-      console.log(error);
-    });
+    JobsApi.getJob(jobid)
+      .then((response) => {
+        job.value = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
-  AuthentificationApi.getMe(decoded.id).then((response) => {
-    me.value = response.data;
-  }).catch((error) => {
-    console.log(error);
-  });
-  CompanyApi.getCompany(route.params.id).then((response) => {
-    jobsCount.value = response.data.jobAds.length;
-  }).catch((error) => {
-    console.log(error);
-  });
+  AuthentificationApi.getMe(decoded.id)
+    .then((response) => {
+      me.value = response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  CompanyApi.getCompany(route.params.id)
+    .then((response) => {
+      jobsCount.value = response.data.jobAds.length;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 let isEmployer = computed(() => {
@@ -140,37 +100,49 @@ const isNumber = computed(() => {
   return /^\d+$/.test(job.value.salary);
 });
 
+const checkNotEmptyForm = () => {
+  return (
+    job.value.title != '' &&
+    job.value.description != '' &&
+    job.value.city != '' &&
+    job.value.country != '' &&
+    job.value.contractType != '' &&
+    job.value.salary != null &&
+    job.value.missionDuration != ''
+  );
+};
 
-let contractTypes = [
-  'Full Time',
-  'Part Time',
-  'Internship',
-  'Freelance',
-  'Temporary',
-  'Apprenticeship',
-];
+let contractTypes = ['Full Time', 'Part Time', 'Internship', 'Freelance', 'Temporary', 'Apprenticeship'];
 
 const addJob = () => {
   disableButton.value = true;
-  if (displayPremium) {
+  if (!checkNotEmptyForm()) {
+    snackbar.value = true;
+    snackbarText.value = 'Veuillez remplir tous les champs';
+    snackbarColor.value = 'error';
+    disableButton.value = false;
+    return;
+  } else if (displayPremium.value) {
     snackbar.value = true;
     snackbarText.value = 'You need to be premium to add more than 2 jobs';
     snackbarColor.value = 'error';
     disableButton.value = false;
     return;
-  } else if (isNumber) {
+  } else if (!isNumber.value) {
     snackbar.value = true;
     snackbarText.value = 'Salary must be a number';
     snackbarColor.value = 'error';
     disableButton.value = false;
     return;
   } else {
-    CompanyApi.createJob(job.value).then((response) => {
-      router.push(`/employer/company/${route.params.id}/jobs`);
-    }).catch((error) => {
-      console.log(error);
-      disableButton.value = false;
-    });
+    CompanyApi.createJob(job.value)
+      .then((response) => {
+        router.push(`/employer/company/${route.params.id}/jobs`);
+      })
+      .catch((error) => {
+        console.error(error);
+        disableButton.value = false;
+      });
   }
 };
 
@@ -184,16 +156,18 @@ const updateJob = () => {
     country: job.value.country,
     contractType: job.value.contractType,
     salary: job.value.salary,
-    missionDuration: job.value.missionDuration,
-  }).then((response) => {
-    if (isEmployer) {
-      router.push(`/employer/company/${route.params.id}/jobs`);
-    } else {
-      router.push(`/admin/jobs`);
-    }
-  }).catch((error) => {
-    console.log(error);
-    disableButton.value = false;
-  });
+    missionDuration: job.value.missionDuration
+  })
+    .then((response) => {
+      if (isEmployer) {
+        router.push(`/employer/company/${route.params.id}/jobs`);
+      } else {
+        router.push(`/admin/jobs`);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      disableButton.value = false;
+    });
 };
 </script>
