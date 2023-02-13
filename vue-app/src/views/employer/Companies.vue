daf0b4c218322fd3272bb9b665fcdd730d770075
Boukhatem <mboukhatem777@gmail.com>
2023-02-13 04:01:27 +0100

    Companies reformatage du code

--- vue-app/src/views/employer/Companies.vue
+++ vue-app/src/views/employer/Companies.vue
@@ -12,66 +12,51 @@
       <p>Adresse: {{ company.address }}</p>
       <p>Website: {{ company.website }}</p>
       <p>Crée le: {{ convertDate(company.creationDate) }}</p>
-      <p> Chiffre d'affaire: {{ company?.revenue?.revenue }}</p>
+      <p>Chiffre d'affaire: {{ company?.revenue?.revenue }}</p>
       <p>Secteur: {{ company?.sector?.sector }}</p>
       <p>Nombre d'employé: {{ company?.size?.size }}</p>
       <p>Fondateur: {{ company?.founder?.firstname }} {{ company?.founder?.lastname }}</p>
       <div>
         Offres d'emploi: 
         <v-btn
-        @click="this.$router.push(`/employer/company/${company.id}/jobs`)"
-        icon="mdi-briefcase"
+          @click="goToJobsPage"
+          icon="mdi-briefcase"
         />
       </div>
     </v-card-text>
   </v-card>
 </template>
-
-<script setup>
+<script>
 import { ref, onMounted, computed } from 'vue'
 import CompanyApi from '@/backend/CompanyApi'
 import AuthentificationApi from '@/backend/AuthentificationApi';
 import BaseNaveBar from '@/components/BaseNaveBar.vue'
 import jwt_decode from 'jwt-decode';
 
-let me = ref({});
-const token = localStorage.getItem('token');
-const decoded = jwt_decode(token);
-
-const getUser = async () => {
-  await AuthentificationApi.getMe(decoded.id).then((response) => {
-    me.value = response.data;
-    companyId.value = me.value.companies[0].id;
-  }).catch((error) => {
-    console.log(error);
-  });
-}
-
+const decoded = jwt_decode(localStorage.getItem('token'));
+const me = ref({});
 const companyId = ref(null);
-let company = ref({});
-onMounted( async () => {
-  // wait until getUser is done
-  await getUser();
-  await CompanyApi.getCompany(companyId.value).then((response) => {
-    console.log(response);
-    company.value = response.data;
-    console.log(company);
-  }).catch((error) => {
-    console.log(error);
-  });
-});
-const isEmployer = computed(() => {
-  return decoded.roles.includes('ROLE_EMPLOYER');
-});
+const company = ref({});
+const isEmployer = computed(() => decoded.roles.includes('ROLE_EMPLOYER'));
+const companies = computed(() => me.value.companies);
 
-let companies = computed(() => {
-  return me.value.companies;
+onMounted(async () => {
+  try {
+    me.value = (await AuthentificationApi.getMe(decoded.id)).data;
+    companyId.value = me.value.companies[0].id;
+    company.value = (await CompanyApi.getCompany(companyId.value)).data;
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 const convertDate = (date) => {
-  const d = new Date(date)
-  d.setHours(d.getHours() - 1)
-  return d.toLocaleDateString('fr-FR')
-}
+  const d = new Date(date);
+  d.setHours(d.getHours() - 1);
+  return d.toLocaleDateString('fr-FR');
+};
 
+const goToJobsPage = () => {
+  this.$router.push(`/employer/company/${company.id}/jobs`);
+};
 </script>
